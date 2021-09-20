b48f79ed447d1779b8a116379b8681968d5260c5
windows10win7posreadywoeskio <minminramin2@gmail.com>
2021-09-20 15:00:22 +0700

    shit

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -311,7 +311,7 @@ class TitleState extends MusicBeatState
 					}
 					else
 					{
-						track('everything is k (' + returnedData[0] + '=' + MainMenuState.kadeEngineVer + ')')
+						track('everything is k (' + returnedData[0] + '=' + MainMenuState.kadeEngineVer + ')');
 						FlxG.switchState(new MainMenuState());
 						clean();
 					}
